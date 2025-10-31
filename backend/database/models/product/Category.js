4792ae5ecdea282ea61efa683842e6a404330075
FTass <ftassgaj@mgail.com>
2025-10-31 13:54:16 -0300

    feat: servicio para categorias listo

--- backend/database/models/product/Category.js
+++ backend/database/models/product/Category.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const categorySchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    slug: { type: String, required: true, unique: true },
+    name: { type: String, required: true, trim: true },
+    slug: { 
+        type: String, 
+        required: true, 
+        unique: true,
+        trim: true,
+        lowercase: true
+    },
     description: { type: String, required: true },
     parentId: {
         type: mongoose.Schema.Types.ObjectId,
